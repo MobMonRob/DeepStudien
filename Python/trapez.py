a2e1bc1dea290f42149717af8b8c28023d1cabec
D064518 <enrico.kaack@sap.com>
2018-01-26 09:25:49 +0100

    Implemented trapez integration filter

--- Python/trapez.py
+++ Python/trapez.py
@@ -2,52 +2,91 @@
 """
 Created on Thu Jan 25 17:11:23 2018
 
-@author: D064868
+@author: Benedikt Bosshammer
 """
+from pyquaternion import Quaternion
 
+
+class TrapezIntegration:
+
+    def __init__(self, p_init=[0,0,0], v_init=[0,0,0]): 
+        self.o_prev = None
+        self.v_prev = v_init
+        self.a_prev = None
+
+        self.p_prev = p_init
+    
+    def update(self, o, acc, dt):
+        
+        if (o_prev == None):
+            self.o_prev = o
+        else:
+            self.o_prev = meanOfQuaternions(o, self.o_prev)
+            
+
+        #rotate acc to fit to orientation
+        acc = o.rotate(acc)
+
+        # Update velocity with time and acceleration
+        if (self.a_prev == None):
+            vel = add(self.v_prev, multiplyByScale(sub(acc, [0,0,9.81]), dt))
+        else:
+            vel = add(self.v_prev, multiplyByScale(add(sub(acc, [0,0,9.81]), sub(self.a_prev, [0,0,9.81])), dt/2))
+
+        self.a_prev = acc
+
+        # Positio update using trapez integration over velocity
+        p = add(self.p_rev, multiplyByScale(add(vel, self.v_prev), dt/2))
+
+        self.p_prev = p
+        self.v_prev = vel
+
+        return p
+
+    @staticmethod
+    def add(a, b):
+        return [a[0]+b[0], a[1]+b[1], a[2]+b[2]]
+
+    @staticmethod
+    def sub(a, b):
+        return [a[0]-b[0], a[1]-b[1], a[2]-b[2]]  
+
+    @staticmethod
+    def multiplyByScale(a, b):
+        return [a[0]*b, a[1]*b, a[2]*b[2]]
+
+    @staticmethod
+    def meanOfQuaternions(a, b):
+        #Create quaternion
+        aElements = a.elements
+        bElements = b.elements
+        temp = []
+        for i in (0,3):
+            temp[i] = 0.5 * (aElements[i] + bElements[i])
+
+        return Quaternion(temp)        
+        
 data = json.load(open('../Sensordaten/konvertiert/' + PATH + '.json'))
 
-ori = 0.0
-o_prev = 0.0
+trapez = TrapezIntegration()
 
-v_prev = 0.0
-vel = 0.0
+time_prev = 0
+for i in range(0, data.length):
+    entry = data[i]
 
-a_prev = 0.0
-acc = 0.0
+    # Read orientation
+    orientRot = [entry.get("orient_x"), entry.get("orient_y"), entry.get("orient_z") ]
+    o = Quaternion(axis=orientRot, degrees=90)
 
-pos = 0.0
-p_prev = 0.0
+    # Read acc
+    acc = [entry.get("acc_x"), entry.get("acc_y"), entry.get("acc_z")]
 
-orient_x = 0
-orient_y = 0
-orient_z = 0
+    dt = entry.get("time") - time_prev
+    time_prev = entry.get("time")
 
-# euler vector (orient_x, orient_y, orient_z) to quarternion
+    trapez.update(o, acc, dt)
+    position = trapez.p
+    print(position)
 
-for x in range(0, 1):
 
-    entry = data[x]
-    
-    #orient_x = entry.get("orient_x")
-    #orient_y = entry.get("orient_y")
-    #orient_z = entry.get("orient_z")
-    
-# set initial p_prev and v_prev
 
-### update function for every measurement
-
-# update orientation
-# -> mean of o and o_new    
-    
-# rotation acc to ori
-    
-# set a_prev
-
-# Zeile 51
-    
-# a_prev = a
-    
-# p = p_prev + (v(t) + v(t-1) * dt/2)
-
-# v_prev = v
