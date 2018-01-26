024f92664ad9c06fbf31d9e3c3228f01a772410d
D064518 <enrico.kaack@sap.com>
2018-01-26 09:57:41 +0100

    Fixed syntax and compile bugfs

--- Python/trapez.py
+++ Python/trapez.py
@@ -5,23 +5,26 @@ Created on Thu Jan 25 17:11:23 2018
 @author: Benedikt Bosshammer
 """
 from pyquaternion import Quaternion
+import json
+import sys
 
 
 class TrapezIntegration:
-
+    o_prev = Quaternion()
+    v_prev = None
+    a_prev = None
+    p_prev = None
     def __init__(self, p_init=[0,0,0], v_init=[0,0,0]): 
-        self.o_prev = None
+        
         self.v_prev = v_init
-        self.a_prev = None
-
         self.p_prev = p_init
-    
+
     def update(self, o, acc, dt):
         
-        if (o_prev == None):
+        if (self.o_prev):
             self.o_prev = o
         else:
-            self.o_prev = meanOfQuaternions(o, self.o_prev)
+            self.o_prev = self.meanOfQuaternions(o, self.o_prev)
             
 
         #rotate acc to fit to orientation
@@ -29,14 +32,14 @@ class TrapezIntegration:
 
         # Update velocity with time and acceleration
         if (self.a_prev == None):
-            vel = add(self.v_prev, multiplyByScale(sub(acc, [0,0,9.81]), dt))
+            vel = self.add(self.v_prev,self.multiplyByScale(self.sub(acc, [0,0,9.81]), dt))
         else:
-            vel = add(self.v_prev, multiplyByScale(add(sub(acc, [0,0,9.81]), sub(self.a_prev, [0,0,9.81])), dt/2))
+            vel = self.add(self.v_prev, self.multiplyByScale(self.add(self.sub(acc, [0,0,9.81]), self.sub(self.a_prev, [0,0,9.81])), dt/2))
 
         self.a_prev = acc
 
         # Positio update using trapez integration over velocity
-        p = add(self.p_rev, multiplyByScale(add(vel, self.v_prev), dt/2))
+        p = self.add(self.p_prev, self.multiplyByScale(self.add(vel, self.v_prev), dt/2))
 
         self.p_prev = p
         self.v_prev = vel
@@ -53,7 +56,7 @@ class TrapezIntegration:
 
     @staticmethod
     def multiplyByScale(a, b):
-        return [a[0]*b, a[1]*b, a[2]*b[2]]
+        return [a[0]*b, a[1]*b, a[2]*b]
 
     @staticmethod
     def meanOfQuaternions(a, b):
@@ -65,27 +68,28 @@ class TrapezIntegration:
             temp[i] = 0.5 * (aElements[i] + bElements[i])
 
         return Quaternion(temp)        
-        
+
+
+PATH = "ruhig"
 data = json.load(open('../Sensordaten/konvertiert/' + PATH + '.json'))
 
 trapez = TrapezIntegration()
 
 time_prev = 0
-for i in range(0, data.length):
+for i in range(0, len(data)):
     entry = data[i]
 
     # Read orientation
-    orientRot = [entry.get("orient_x"), entry.get("orient_y"), entry.get("orient_z") ]
+    orientRot = [float(entry.get("orient_x")), float(entry.get("orient_y")), float(entry.get("orient_z")) ]
     o = Quaternion(axis=orientRot, degrees=90)
 
     # Read acc
-    acc = [entry.get("acc_x"), entry.get("acc_y"), entry.get("acc_z")]
+    acc = [float(entry.get("ac_x")), float(entry.get("ac_y")), float(entry.get("ac_z"))]
 
-    dt = entry.get("time") - time_prev
-    time_prev = entry.get("time")
+    dt = int(entry.get("time")) - time_prev
+    time_prev = int(entry.get("time"))
 
-    trapez.update(o, acc, dt)
-    position = trapez.p
+    position = trapez.update(o, acc, dt)
     print(position)
 
 
