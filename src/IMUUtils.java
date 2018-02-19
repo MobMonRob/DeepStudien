14d33118997ee7acee31401cdca0ecf9e79b98c2
Oliver Rettig <Oliver.rettig@orat.de>
2018-02-19 16:46:34 +0100

    Update IMUUtils.java

--- src/IMUUtils.java
+++ src/IMUUtils.java
@@ -13,7 +13,13 @@ public class IMUUtils {
       c[1][1] = d;
       c[2][2] = d;
       
-      //double[][] s = MatrixUtils
+      MatrixUtils.add(c,VectorUtils(new double[]{2*o[0], 2*o[1], 2*o[3]},new double[]{o[0],o[1],o[2]});
+         
+      double[][] e = new double[3][3];
+      e[0][1] = 
+                      
+      MatrixUtils.add(c,e);
+                      
       double[] result = new double[4];
       
       //TODO
